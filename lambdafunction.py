be816028e2a498ee2275e3fd9c10bd43da163d0c
ZacharyLinton <111541337+ZacharyLinton@users.noreply.github.com>
2023-02-17 21:12:52 -0600

    Update lambdafunction.py

--- lambdafunction.py
+++ lambdafunction.py
@@ -1,28 +1,44 @@
-import json
 import boto3
+import json
 
 def lambda_handler(event, context):
-    # Get the data from the form
-    name = event.get('name')
-    address = event.get('address')
-    email = event.get('email')
-    phone = event.get('phone')
-
-    # Store the data in a DynamoDB table
+    # Create a DynamoDB resource
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('signup')
-    table.put_item(
-        Item={
-            'Name': name,
-            'Address': address,
-            'Email': email,
-            'Phone': phone
-        }
-    )
 
-    # Return a success response
-    response = {
-        'statusCode': 200,
-        'body': json.dumps('Sign up successful!')
-    }
+    try:
+        # Get the request body from the event
+        request_body = json.loads(event['body'])
+
+        # Extract the customer information from the request body
+        name = request_body['name']
+        address = request_body['address']
+        phone = request_body['phone']
+        email = request_body['email']
+
+        # Insert the customer information into the DynamoDB table
+        table = dynamodb.Table('customers')
+        table.put_item(
+            Item={
+                'name': name,
+                'address': address,
+                'phone': phone,
+                'email': email
+            }
+        )
+
+        # Return a success response
+        response = {
+            'statusCode': 200,
+            'body': json.dumps('Customer information saved successfully')
+        }
+    except Exception as e:
+        # Log the error
+        print(str(e))
+
+        # Return an error response
+        response = {
+            'statusCode': 500,
+            'body': json.dumps('An error occurred while saving customer information')
+        }
+
     return response
